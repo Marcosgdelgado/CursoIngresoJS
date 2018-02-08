83198b03dde05e9caa35276b0d0e92c1ef6daa53
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-08 20:40:10 -0300

    ejercicio 1 y 2
    alert, promt, variables, valores literales, undefined, notas marginales.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	// defino una variable 
+	var nombre;
+	//alert(nombre);
+	nombre=prompt("ingrese su nombre","Ragnar");
+	alert(nombre);
+
+	/*
+	nombre="Marcos";
+	alert(nombre);
+	alert("nombre")
+	*/ 
+	// var person =prompt("Please enter your name", "Harry Potter");
 }
 
+/* alert (" ")
+promt (" ") si o si con un = antes con una variante declarada.
+nombre=prompt("ingrese su nombre","Chiquita"); completa automaticamente el promt.
+Undefined = indefinido, controlar codigo
+
+*/
