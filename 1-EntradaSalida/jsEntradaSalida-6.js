0f706ef65aa51d62b0f4f1c4c0d5f83042d98501
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-09 19:49:26 -0300

    Ejercicio 6 (terminado)

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,14 +2,30 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
-{
-	var numeroUno;
-	var numeroDos;
+{	var num1;
+	var num2;
+	var suma;
+
+	num1= document.getElementById('numeroUno').value;
+	num2= document.getElementById('numeroDos').value;
+	num1= parseInt(num1);
+	num2= parseInt(num2);
+
+	suma= num1+num2;
+	alert("la suma es " +suma);
+}
+
+/*var numeroUno=1;
+	var numeroDos=2;
 	var resultado;
 
 	numeroUno=document.getElementById('numeroUno').value;
 	numeroDos=document.getElementById('numeroDos').value;
 	
 
-}
+	var numeroUno=parseInt("1");
+	var numeroDos=parseInt("2");
+	var resultado= numeroUno+numeroDos;
 
+
+	alert("el resultado es "+resultado);*/
