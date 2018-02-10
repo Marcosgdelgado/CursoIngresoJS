d1eb5f51d709bc577436ad43d1de3c22eb30dd1f
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-09 21:59:33 -0300

    Ejercicio 1,2 y 3
    If y Else

---
+++ 2-InstruccionIf/uno.js
@@ -0,0 +1,17 @@
+
+function Mostrar()
+{
+	var num1;
+	var num2;
+	var resultado;
+
+	num1=document.getElementById('laBase').value;
+	num1=parseInt(num1);
+	
+	num2=4;
+	resultado=num1*num2;
+
+
+	alert("El perimetro del cuadrado es "+resultado);
+
+}
