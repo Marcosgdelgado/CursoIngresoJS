154fb8ca3f51e51e3d60bfc31f6a99b5fa69cbc5
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-11 16:12:27 -0300

    Parcial 2 (sin terminar)
    dificultad en el desarrollo

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
   
+    var a;
+    var b;
+    var resultado;
+    
+ 
+  	prompt("Ingrese un importe");
+	a=document.getElementById('importeFinal').value;
+	a=parseInt(a);
+    
+    b=0.21;
+    resultado= a*b;
+	document.getElementById('importeFinal').value=resultado;
+	parseInt(resultado);
 }
