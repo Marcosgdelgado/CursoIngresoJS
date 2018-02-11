3ddc18153eae16f561711e5d582fa3e33888e118
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-11 15:05:34 -0300

    Ejercicio 2,3 y 4
    geyById, if, else, parseInt, document

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad 
+
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+
+if(edad<=17&&edad>13)
+{
+	alert("Es adolesente ");
+}
+
+else
+{
+	alert("no es adolesente");
+}
 
 
 
