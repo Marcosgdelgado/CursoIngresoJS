10c4baa84415ae62e7b0b80ff4a49f3760fa43af
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-11 15:41:47 -0300

    Ejercicio 7 (inconcluso)

---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad
+var edad
+var estado
+
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+estado=document.getElementById('estadoCivil').value;
+
+if(edad<18)
+{
+	alert("Es muy pequeño para no ser soltero ");
+}
+
+
+	
+
+
+}//FIN DE LA FUNCIÓN
