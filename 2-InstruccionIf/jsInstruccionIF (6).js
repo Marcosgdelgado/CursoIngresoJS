dd17eee45a370e36df274435c2e33b1526303e4f
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-11 15:40:57 -0300

    Ejercicios 5 y 6
    If, else, &&, ||, <, >

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,24 @@
 function Mostrar()
 {
-//tomo la edad  
+var edad
 
+edad=document.getElementById('edad').value;
+edad=parseInt(edad);
+
+
+if(edad>=18)
+{
+alert("Es mayor");
+} 
+if(edad<=17&&edad>=13)
+{
+	alert("Es adolescente");
+}
+else
+{alert("Es niño");
+}
 
 
 
 }//FIN DE LA FUNCIÓN
+/*el codigo funciona, pero creo que se puede resumir.*/
