d439681163c36af9bea25f1b47591321504964b1
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-11 16:11:34 -0300

    Parcial 1
    terminado

--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,16 @@
 
 function Mostrar()
 {
+var num1
+var num2
+var resultado
 
+num1=document.getElementById('laBase').value;
+num1=parseInt(num1);
+
+num2=4;
+
+resultado=num1*num2;
+
+alert("El perimetro es "+resultado);
 }
