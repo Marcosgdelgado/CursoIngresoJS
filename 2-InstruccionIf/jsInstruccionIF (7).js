c48a71de7c871e73d70b6e4fa7f677cc917f1e22
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-15 20:58:17 -0300

    Ejercicio 7 y 8
    if, getby

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,20 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad
+ 	var estadoCivil
+ 
+	edad=document.getElementById('edad').value;
+ 	edad=parseInt(edad);
+ 
+ 	estadoCivil=document.getElementById('estadoCivil').value;
 
-	
+ 
+	 if(edad<18 && estadoCivil!="Soltero")
+ 
+ 	{
+ 		alert("Es muy pequeño para no ser soltero ");
+ 	}
 
 
 }//FIN DE LA FUNCIÓN
