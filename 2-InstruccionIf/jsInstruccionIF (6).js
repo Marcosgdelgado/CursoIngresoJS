0b3a8d29b6d5a6ec64e1229f79f007b9acb808a2
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-15 20:58:48 -0300

    Ejercicio 6 (corregido)

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -6,7 +6,23 @@ edad=document.getElementById('edad').value;
 edad=parseInt(edad);
 
 
-if(edad>=18)
+if (edad<13)
+{
+	alert("niño");
+}
+else
+{
+	if(edad>17)
+	{
+		alert("mayor");
+
+	}
+	else
+	{
+		alert("adolescente");
+	}
+}
+/*if(edad>=18)
 {
 alert("Es mayor");
 } 
@@ -15,10 +31,34 @@ if(edad<=17&&edad>=13)
 	alert("Es adolescente");
 }
 else
-{alert("Es niño");
+{	alert("Es niño");
+}*/
+
 }
 
-
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
 /*el codigo funciona, pero creo que se puede resumir.*/
+/*programa falla cuando ingreso una edad mayor a 18*/
+/*if(edad>=18)
+{
+alert("Es mayor");
+} 
+if(edad<=17&&edad>=13)
+{
+	alert("Es adolescente");
+}
+else
+{	alert("Es niño");
+}*/
+/* if(edad>=18)
+{
+alert("Es mayor");
+} 
+if(edad<=17&&edad>=13)
+{
+	alert("Es adolescente");
+}
+if(edad<13)
+{	alert("Es niño");
+}
+USAR TRES IF COMO ALTERNATIVA*/
