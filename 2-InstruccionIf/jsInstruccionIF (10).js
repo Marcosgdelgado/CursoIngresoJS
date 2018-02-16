b248a96231d476008760600ac5e5b76c41148ff1
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-15 21:42:34 -0300

    Ejercicio 9 y 10
    numero RANDOM, Math.floor, alert(numeroRandom)

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,43 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom
 
-}//FIN DE LA FUNCIÓN
+	numeroRandom= document.getElementById('FormIngreso').innerHTML;
+	numeroRandom= Math.floor(Math.random() * 10) + 1;
+
+	if(numeroRandom>=9)
+	{
+		alert(numeroRandom);
+		alert("Excelente");
+	}
+	if (numeroRandom>4 && numeroRandom<9)
+	{
+		alert(numeroRandom);
+		alert("Aprobo");
+
+	}
+	if (numeroRandom<3)
+	{
+		alert(numeroRandom);
+		alert("¡Vamos! la proxima se puede");
+	}
+}
+	/*if(numeroRandom<4)
+	{
+		alert(numeroRandom);
+		alert("¡Vamos! la proxima se puede");
+	}
+
+		if(numeroRandom>4||numeroRandom<9)
+		{
+			alert(numeroRandom);
+			alert("Aprobo");
+		}
+		if(numeroRandom<9)
+		{
+			alert(numeroRandom);
+			alert("Excelente");
+		}
+}
+	//FIN DE LA FUNCIÓN*/
