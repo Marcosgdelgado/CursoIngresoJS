0838cf6aa9bc3f60cae49b0e28e4ba003b87296f
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-16 21:31:40 -0300

    Parcial 4 (terminado)

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,42 @@
 function Mostrar()
 {
+	var numeroUno
+	var numeroDos
+	var resultado
+	
+	numeroUno=prompt("Ingrese un numero");
+	numeroUno=parseInt(numeroUno);
+	
+	numeroDos=prompt("Ingrese un numero");
+	numeroDos=parseInt(numeroDos);
 
-}
+	if (numeroUno==numeroDos)
+	{
+		resultado=numeroUno*numeroDos;
+
+	}
+	else 
+	{
+		if (numeroUno>numeroDos)
+		{
+			resultado=numeroUno-numeroDos;
+		}
+		else
+		{
+			resultado=numeroUno+numeroDos;
+		}
+
+	}
+
+		document.write(resultado);
+	}
+
+
+
+
+
+
+
+
+
+
