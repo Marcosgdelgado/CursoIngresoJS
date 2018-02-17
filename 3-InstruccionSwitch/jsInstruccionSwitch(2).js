284eee76ce53460df98df51e92ecc1fc29eb4f97
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-16 21:46:50 -0300

    Ejercicio 2 y 3
    switch, case, break y default

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,7 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch (mesDelAño)
+{
+	case "Julio":
 
+	case "Agosto":
+	 		alert("Abrigrate que hace frio");
+	 		break;
+
+	case "Enero":
+	
+	case "Febrero":
+
+	case "Marzo":
+
+	case "Junio":
+			alert("Ya paso el frio! ahora viene el calor!");
+			break; 		
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -3,9 +3,17 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+
+switch(mesDelAño)
+{
+	case "Febrero":
+				alert("Este mes tiene 29 días");
+					break;	
+
+	default:
+		alert("Este mes tiene 30 días o más");
+}	
 	
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN. DEFAULT dentro del switch no abrir {}
