ec0efcc4851ee44977534ba832f98fe500f43a10
Marcosgdelgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-02-16 22:09:28 -0300

    Ejercicio 4, 5 y 6
    Switch, case, break, default

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -3,7 +3,15 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+switch(laHora)
+{
+	case "7":
+	case "8":
+	case "9":
+	case "10":
+	case "11":
+		alert(laHora+" es de mañana");
+}
 	
 	
 
