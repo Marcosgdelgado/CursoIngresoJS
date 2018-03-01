cec99b8f08b379cf9bfe7ec83e3328b98ed3b20d
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-03-01 19:07:19 -0300

    ejercicio 3(terminado)

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,20 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var resultado;
+
+	largo=document.getElementById('alrgo').value;
+	largo=parseInt(largo);
+
+	ancho=document.getElementById('ancho').value;
+	ancho=parseInt(ancho);
+
+	resultado=largo*2+ancho*2;
+
+	resultado=resultado*3;
+	
+	alert("se necesitan "+resultado+" metros de alambre");
+
 
 }
