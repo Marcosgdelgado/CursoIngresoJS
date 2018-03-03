c5833798fd6e9787d81c598b54e78e3a229bc5b3
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-03-03 13:11:55 -0300

    Parcial 8 terminado

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -3,7 +3,7 @@ function Mostrar()
 //los datos son: nombre del animal, peso del animal(mayor a 0) y temperatura del habitat(entre -40 a +40).
 //informar: nombre del animal mas pesado, nombre del animal de la temp mas baja, 
 //la cantidad de temp pares que se ingresaron, promedio de peso de todos los animales, temp max y min.
-{
+/*{
 	var nombreAnimal;
 	var pesoAnimal;
 	var temperaturaHabitat;
@@ -11,50 +11,122 @@ function Mostrar()
 	var minTemperatura;
 	var temperaturaPar;
 	var respuesta="si";
-	var contador;
+	var contador=0;
+	var temperaturaPar;
+	var temperatudaMin;
 	
 
-	
-	while(respuesta!="no")
+	for(i=0; ; i++)
 	{
-		contador++;
-		nombreAnimal=prompt("Ingrese nombre del animal: ");
+		while(respuesta!="no")
+		{			
+			nombreAnimal=prompt("Ingrese nombre del animal: ");
 	
-		pesoAnimal=prompt("Ingrese peso del animal: ");
-		pesoAnimal=parseInt(pesoAnimal);
-
-		temperaturaHabitat=prompt("Ingrese temperatura del habitad: ");
-		temperaturaHabitat=parseInt(temperaturaHabitat);
-
-		while(isNaN(pesoAnimal)|| pesoAnimal>0)
-		{
-			pesoAnimal=prompt("Ingrese peso por favor");
+			pesoAnimal=prompt("Ingrese peso del animal: ");
 			pesoAnimal=parseInt(pesoAnimal);
-		}
-		while(isNaN(temperaturaHabitat)|| temperatuda<-41 || temperatuda>40)
-		{
-			temperaturaHabitat=prompt("Ingrese una temperatuda por favor");
+
+			temperaturaHabitat=prompt("Ingrese temperatura del habitad: ");
 			temperaturaHabitat=parseInt(temperaturaHabitat);
-		}
+
+			while(isNaN(pesoAnimal) && pesoAnimal>0)
+			{
+				pesoAnimal=prompt("Ingrese peso por favor");
+				pesoAnimal=parseInt(pesoAnimal);
+			}
+				while(isNaN(temperaturaHabitat) || temperaturaHabitat<-41 || temperaturaHabitat>40)
+				{
+					temperaturaHabitat=prompt("Ingrese una temperatura por favor");
+					temperaturaHabitat=parseInt(temperaturaHabitat);
+				}
 	
 		
-		if(contador==1)
-		{
-			nombrePesado=nombre;
-			pesopesado=peso;
-			minTemperatura=temperatuda;
-			nombrefrio=nombre;
-			temperatudamax=temperatuda;
-		}
-		else
-		{
-			if()
-		}
+					if(contador==0)
+					{
+						contador++;
+						pesoAnimal=nombreAnimal;
+						pesopesado=peso;
+						minTemperatura=temperaturaHabitat;
+						nombrefrio=nombre;
+						temperatudamax=temperatuda;
+					}
+				else
+			{
+				if()
+			}
 
 		
 
-		respuesta=prompt("Desea continuar ?");
+			respuesta=prompt("Desea continuar ?");
+		}
+
+	}	
+		document.write(nombreAnimal+ "El peso maximo es "+pesoAnimal+" La temperatura de su habitat es: "+temperaturaHabitat);
+	
+}*/
+{
+	var numero;
+	var seguir=true;
+	var cantPares=0;
+	var contador=0;
+	var acumulador=0;
+	var flag=false;
+	var maximo;
+	var minimo;
+	var promedio;
+
+	while(seguir==true)
+	{	
+		numero=prompt("Ingrese un numero"),
+		numero=parseInt(numero);
+
+		while(numero<=0)
+		{
+			numero=prompt("ingrese un numero");
+			numero=parseInt(numero);
+
+		}
+
+		if(numero%2==0)
+		{
+			cantPares++;
+		}
+
+		acumulador+=numero;
+		if(flag==false|| numero<minimo)
+		{
+			minimo=numero;
+			flag=true;
+		}
+
+		contador++;
+		seguir=confirm("desea ingresar otro?");
+
+
 	}
 
-	document.write(nombreAnimal+ "El peso maximo es "+pesoAnimal);
+	promedio=acumulador/contador;
+
+	document.write("promedio: "+ promedio+"<br>");
+	document.write("sumatoria: "+acumulador+"<br>");
+	document.write("maximo: "+maximo+ "minimo: "+minimo+"<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
