d4c940fe657e6cd46f1d5a1599d5df7785cb764d
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-03-02 22:18:19 -0300

    Parcial 7 sin terminar

--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,43 @@
+
+
 function Mostrar()
 {
+	var nota;
+	var sexo;
+	var acumulador=0;
+	var contador=0;
+	
+	prompt=("Ingrese 'm' para masculino o 'f' para femenino: " );
+	for(i=0; i>6; i++)
+	{
+		while( sexo!='f'&& sexo!='m')
+		{
+			sexo=prompt("Ingrese sexo");
+		}
 
+		while(nota<0||nota>10)
+		{
+			nota=prompt("ingrese un numero: ");
+			nota= +(nota);
+		}
+
+		acumulador+=nota;
+		if(i==0)
+		{
+			minimo=nota;
+		}
+		if(nota>5 %% sexo=='m')
+		{
+			contador++;
+		}
+	}
 }
+
+
+
+
+/*Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) 
+ el sexo (validar el sexo “f” o “m”) de 100 alumnos, informar por alert:
+  a) el promedio de las notas totales. 
+  b) la nota más baja. 
+  c) la cantidad de varones que su nota haya sido mayor o igual a 6.*/
