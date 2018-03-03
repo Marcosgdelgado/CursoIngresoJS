d4a1d2e5a378654c391a365a1f3d5fab90a5afc1
Marcos Delgado <36281537+Marcosgdelgado@users.noreply.github.com>
2018-03-02 21:53:31 -0300

    Parcial 5 (versión final)

---
+++ cinco.js
@@ -0,0 +1,57 @@
+function Mostrar()
+
+/*{
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var resultado;
+
+	numeroUno=prompt("Ingrese un numero:");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("Ingrese un numero:");
+	numeroDos=parseInt(numeroDos);
+
+	numeroTres=prompt("Ingrese un numero:");
+	numeroTres=parseInt(numeroTres);
+
+	if(numeroUno>numeroDos&numeroTres)
+	{
+		resultado=numeroUno;
+	}
+	
+	else if(numeroDos>numeroUno&numeroDos>numeroTres)
+	{
+
+		resultado=numeroDos;
+		
+	} 
+		else
+		{
+			resultado=numeroTres;
+		}
+	
+		alert("El mayor es: "+resultado);
+}*/
+
+
+
+
+
+{
+	var dia;
+	dia=prompt("Ingrese día: ");
+	dia= díiaa.toLowerCase();
+	
+
+	switch(dia)
+	{
+		case "sabado":
+		case "domingo":
+			alert("Es fin de semana!!");
+				break;
+		default:
+			alert("A trabajar!!");	
+
+	}
+}
